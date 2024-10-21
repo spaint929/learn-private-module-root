4469ddb3ac1eb649b053e2006fee3126e3abbe7e
spaint929 <46006885+spaint929@users.noreply.github.com>
2024-10-21 16:50:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/learn-hashiscott/s3-webapp/aws"
+  source  = "app.terraform.io/learn-hashiscott/s3-webapple/aws"
   name   = var.name
   region = var.region
   prefix = var.prefix
