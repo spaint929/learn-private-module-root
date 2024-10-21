5c03ab9861586c5cd156b3ac7c86867dee39b509
spaint929 <46006885+spaint929@users.noreply.github.com>
2024-10-21 16:11:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 module "s3-webapp" {
-  source  = "app.hashicorp.io/learn-hashiscott/s3-webapp/aws"
-  name    = var.name
-  region  = var.region
-  prefix  = var.prefix
+  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  name   = var.name
+  region = var.region
+  prefix = var.prefix
   version = "1.0.0"
 }
